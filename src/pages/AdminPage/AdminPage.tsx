40de10c734bcfe6dc796803bab2e11a44e22fb4c
lx1cx <lagunov.2019@list.ru>
2023-05-09 02:54:50 +0500

    Рефакторинг

--- src/pages/AdminPage/AdminPage.tsx
+++ src/pages/AdminPage/AdminPage.tsx
@@ -1,32 +1,28 @@
 import React from 'react';
 import styles from "./adminPage.module.css";
+import {Link} from "react-router-dom";
 
 const AdminPage = () => {
     return (
-        <div className={styles.wrapper}>
-            <div className={styles.admin}>
-                <p className={styles.logo}>MeowLib</p>
-                <div className={styles.admin_items}>
-                    <div className={styles.admin_item}>
-                        <p>Книги</p>
-                    </div>
+        <div className={styles.admin_items}>
+            <div className={styles.admin_item}>
+                <p>Книги</p>
+            </div>
 
-                    <div className={styles.admin_item}>
-                        <p>Теги</p>
-                    </div>
+            <div className={styles.admin_item}>
+                <p>Теги</p>
+            </div>
 
-                    <div className={styles.admin_item}>
-                        <p>Авторы</p>
-                    </div>
+            <Link to={"authors"} className={styles.admin_item}>
+                <p>Авторы</p>
+            </Link>
 
-                    <div className={styles.admin_item}>
-                        <p>Пользователи</p>
-                    </div>
+            <div className={styles.admin_item}>
+                <p>Пользователи</p>
+            </div>
 
-                    <div className={styles.admin_item}>
-                        <p>Модерация</p>
-                    </div>
-                </div>
+            <div className={styles.admin_item}>
+                <p>Модерация</p>
             </div>
         </div>
     );
