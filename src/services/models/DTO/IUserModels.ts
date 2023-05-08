72013ee4653758e4ac43a2efb2a6570c67d71b09
lx1cx <lagunov.2019@list.ru>
2023-05-09 02:58:27 +0500

    Изменено значение Enum

--- src/services/models/DTO/IUserModels.ts
+++ src/services/models/DTO/IUserModels.ts
@@ -2,10 +2,10 @@
  * Перечесление возможных ролей пользователя.
  */
 export enum UserRolesEnum {
-    User,
-    Editor,
-    Moderator,
-    Admin
+    User = "User",
+    Editor = "Editor",
+    Moderator = "Moderator",
+    Admin = "Admin"
 }
 
 /**
