1cc2ecad8340d0861ba31d5f9c074b00c1979b90
lx1cx <lagunov.2019@list.ru>
2023-05-09 03:00:29 +0500

    Создан интерфейс для описания возвращаемого значения

---
+++ src/services/models/responses/IAuthorResponse.ts
@@ -0,0 +1,9 @@
+import {IAuthorDTO} from "../DTO/IAuthorModels";
+
+/**
+ * Интерфейс описывающий возвращаемое значение get запроса для авторов
+ */
+
+export interface IGetAuthorsResponse {
+    data: IAuthorDTO[]
+}
