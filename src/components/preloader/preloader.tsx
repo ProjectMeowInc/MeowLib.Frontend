a16f55721049d4562a93d74ee22e62d8c7866c34
lx1cx <lagunov.2019@list.ru>
2023-05-09 02:57:14 +0500

    Адаптирован прелоадер

---
+++ src/components/preloader/preloader.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from "./preloader.module.css"
+
+const Preloader = () => {
+    return (
+        <div className={styles.lds_roller}>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    );
+};
+
+export default Preloader;
