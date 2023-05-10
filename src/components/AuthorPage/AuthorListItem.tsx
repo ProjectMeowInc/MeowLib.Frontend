f4d67a6b44e435270b78bf3d7b731c7e1f6e1c4a
lx1cx <lagunov.2019@list.ru>
2023-05-10 23:49:20 +0500

    Небольшое изменение UI и написана логика для обновления имени пользователя

--- src/components/AuthorPage/AuthorListItem.tsx
+++ src/components/AuthorPage/AuthorListItem.tsx
@@ -1,16 +1,41 @@
 import React from 'react';
 import styles from "./authorListItem.module.css"
+import {IAuthorDTO} from "../../services/models/DTO/IAuthorModels";
+import {AuthorServices} from "../../services/AuthorServices";
+import {AlertService} from "../../services/AlertService";
 
-interface IAuthorListItem {
-    id: number,
-    name: string
-}
+const AuthorListItem = ({id, name}: IAuthorDTO) => {
 
-const AuthorListItem = ({id, name}: IAuthorListItem) => {
+    function ClickHandler () {
+
+        const newName = prompt("Введите имя")
+
+        if (newName === null) {
+            return AlertService.warningMessage("Вы не указали имя автора")
+        }
+
+        AuthorServices.updateAuthor(id, newName).then(err => {
+            if (err !== null) {
+                return AlertService.errorMessage(err.displayMessage)
+            }
+
+            AlertService.successMessage("Автор был успешно обновлён")
+
+            window.location.reload()
+        })
+    }
+
     return (
         <div className={styles.item}>
-            <p className={styles.id}>{id}</p>
-            <p>{name}</p>
+            <div className={styles.left_side}>
+                <p className={styles.id}>{id}</p>
+                <p>{name}</p>
+            </div>
+
+            <div className={styles.right_side}>
+                <p onClick={ClickHandler}>Изменить</p>
+                <p>Удалить</p>
+            </div>
         </div>
     );
 };
