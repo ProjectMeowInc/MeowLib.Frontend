96d7c4eb3d19bbc757e10b5d262177f1af755cec
lx1cx <lagunov.2019@list.ru>
2023-05-10 23:53:40 +0500

    Рефакторинг

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import {Route, Routes} from "react-router-dom";
 import AuthPage from "./pages/AuthPage/AuthPage";
 import AdminPage from "./pages/AdminPage/AdminPage";
 import AuthorPage from "./pages/Authors/AuthorPage";
-import Layout from "./pages/Layout/Layout";
+import AdminLayout from "./pages/AdminLayout/AdminLayout";
 
 const App = () => {
   return (
         <Routes>
             <Route path={"/login"} element={<AuthPage/>}/>
-            <Route path={"/admin"} element={<Layout/>}>
+            <Route path={"/admin"} element={<AdminLayout/>}>
                 <Route path={""} element={<AdminPage/>}/>
                 <Route path={"authors"} element={<AuthorPage/>}/>
             </Route>
