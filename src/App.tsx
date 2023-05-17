4555edcfe847d73600dd252b364f017a671624bf
lx1cx <lagunov.2019@list.ru>
2023-05-17 23:16:17 +0500

    Изменены routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import {Route, Routes} from "react-router-dom";
 import AuthPage from "./pages/AuthPage/AuthPage";
 import AdminPage from "./pages/AdminPage/AdminPage";
-import AuthorPage from "./pages/Authors/AuthorPage";
+import AuthorPage from "./pages/Authors/MainPage/AuthorPage";
 import AdminLayout from "./layouts/AdminLayout/AdminLayout";
 import NotFoundPage from "./pages/Error/NotFoundPage/NotFoundPage";
 import TagsPage from "./pages/Tags/TagsPage";
 import LoadingLayout from "./layouts/LoadingLayout/LoadingLayout";
+import CreateAuthorPage from "./pages/Authors/CreateAuthorPage/CreateAuthorPage";
+import UpdateAuthorPage from "./pages/Authors/UpdateAuthorPage/UpdateAuthorPage";
 
 const App = () => {
   return (
@@ -15,7 +17,11 @@ const App = () => {
                 <Route path={"login"} element={<AuthPage/>}/>
                 <Route path={"admin"} element={<AdminLayout/>}>
                     <Route path={""} element={<AdminPage/>}/>
-                    <Route path={"authors"} element={<AuthorPage/>}/>
+                    <Route path={"authors"}>
+                        <Route path={""} element={<AuthorPage/>}/>
+                        <Route path={"new"} element={<CreateAuthorPage/>}/>
+                        <Route path={":id/edit"} element={<UpdateAuthorPage/>}/>
+                    </Route>
                     <Route path={"tags"} element={<TagsPage/>}/>
                 </Route>
                 <Route path={"*"} element={<NotFoundPage/>}/>
