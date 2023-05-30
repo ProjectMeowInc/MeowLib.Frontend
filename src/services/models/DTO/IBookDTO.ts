ccdab6fac93bd4861ff4b90839ce792e4372bc08
lx1cx <lagunov.2019@list.ru>
2023-05-30 20:06:03 +0500

    Создан интерфейс опсиывающий книгу

--- src/services/models/DTO/IBookDTO.ts
+++ src/services/models/DTO/IBookDTO.ts
@@ -1,7 +1,22 @@
+import {IAuthorDTO} from "./IAuthorModels";
+import {ITag} from "./ITagDTO";
+
+/**
+ * Интерфейс опсиывающий книгу
+ */
+export interface IBook {
+    id: number
+    name: string
+    description: string
+    authors: IAuthorDTO[]
+    tags: ITag[]
+}
+
 /**
  * Интерфейс опсиывающий DTO книги
  */
 export interface IBookDTO {
+    id: number
     name: string
     description: string
 }
