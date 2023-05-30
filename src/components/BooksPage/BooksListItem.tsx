2f573b8d932d482e3c3f6b758c4cb8e9a9cdd1f0
lx1cx <lagunov.2019@list.ru>
2023-05-30 20:07:31 +0500

    Создана главная страница

---
+++ src/components/BooksPage/BooksListItem.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {AlertService} from "../../services/AlertService";
+import styles from "./booksListItem.module.css";
+import {Link} from "react-router-dom";
+import {IBookDTO} from "../../services/models/DTO/IBookDTO";
+import {BookService} from "../../services/BookService";
+
+const BooksListItem = ({id, name, description}: IBookDTO) => {
+    function DeleteHandler () {
+        BookService.deleteBook(id).then(err => {
+            if (err !== null) {
+                return AlertService.errorMessage(err.displayMessage)
+            }
+
+            AlertService.successMessage("Книга был успешно удалён")
+        })
+    }
+
+    return (
+        <div className={styles.item}>
+            <div className={styles.left_side}>
+                <p className={styles.id}>{id}</p>
+                <p>{name}</p>
+            </div>
+
+            <div className={styles.right_side}>
+                <Link to={`${id}/edit`}>Изменить</Link>
+                <p onClick={DeleteHandler}>Удалить</p>
+            </div>
+        </div>
+    );
+};
+
+export default BooksListItem;
