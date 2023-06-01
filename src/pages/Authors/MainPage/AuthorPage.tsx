e7ed96f4229fe96320da2a46946051562318b1d2
lx1cx <lagunov.2019@list.ru>
2023-06-01 11:57:39 +0500

    Сделан метод для отслеживания ввода в input

--- src/pages/Authors/MainPage/AuthorPage.tsx
+++ src/pages/Authors/MainPage/AuthorPage.tsx
@@ -7,6 +7,8 @@ import {IAuthorDTO} from "../../../services/models/DTO/IAuthorModels";
 import Preloader from "../../../components/preloader/preloader";
 import {AlertService} from "../../../services/AlertService";
 import {Link} from "react-router-dom";
+import {ISearchAuthorRequest} from "../../../services/models/requests/IAuthorRequests";
+import {ErrorTypesEnum} from "../../../services/models/IError";
 
 const AuthorPage = () => {
 
@@ -23,6 +25,22 @@ const AuthorPage = () => {
         })
     }, [])
 
+    async function SearchHandler(data: ISearchAuthorRequest) {
+
+        if (data !== undefined) {
+            const response = await AuthorServices.searchAuthorWithParams(data)
+
+            if (ErrorService.isError(response)) {
+                if (response.errorType === ErrorTypesEnum.Critical) {
+                    return AlertService.errorMessage(response.displayMessage)
+                }
+                return AlertService.warningMessage(response.displayMessage)
+            }
+
+            return setAuthorsList(response)
+        }
+    }
+
     return (
         <div className={styles.wrapper}>
             <h1 className={styles.header}>Управление авторами</h1>
@@ -30,6 +48,10 @@ const AuthorPage = () => {
             <Link to={"new"} className={styles.add_button}>
                 <p>Нажмите чтобы добавить нового автора</p>
             </Link>
+
+            <input onChange={ctx => {
+                SearchHandler({name: ctx.target.value})
+            }} className={styles.search} type="text" placeholder={"Введите имя автора"}/>
 
             {authorsList === null
                 ? <Preloader/>
