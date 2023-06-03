0b98ee76a58fb49e3dc7f20094f78fa06a8b7b77
Black Soul <86704101+BlackSoulHub@users.noreply.github.com>
2023-06-03 19:46:09 +0300

    Добавлен пробел

--- src/services/AuthorServices.ts
+++ src/services/AuthorServices.ts
@@ -64,7 +64,7 @@ export class AuthorServices {
             return null
         }
         catch (err: any) {
-            return ErrorService.toServiceError(err,"AuthorService")
+            return ErrorService.toServiceError(err, "AuthorService")
         }
     }
 
