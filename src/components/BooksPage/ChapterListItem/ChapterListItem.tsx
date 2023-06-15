ef1d60d15a24ec05d2d57f2f0a3c71f6e863bfea
lx1cx <lagunov.2019@list.ru>
2023-06-15 13:56:09 +0500

    Создан копонент для вывода списка глав

---
+++ src/components/BooksPage/ChapterListItem/ChapterListItem.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from "./chapterListItem.module.css";
+
+const ChapterListItem = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ChapterListItem;
