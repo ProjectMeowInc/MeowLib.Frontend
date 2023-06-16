57b0fa45009e4ef0ca37118a9f5061a0ae1b6398
lx1cx <lagunov.2019@list.ru>
2023-06-17 03:43:02 +0500

    Сделан компонент списка глав

--- src/components/BooksPage/ChapterListItem/ChapterListItem.tsx
+++ src/components/BooksPage/ChapterListItem/ChapterListItem.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import styles from "./chapterListItem.module.css";
+import {IChapterDTO} from "../../../services/models/DTO/IChapterDTO";
+import {Link} from "react-router-dom";
 
-const ChapterListItem = () => {
+const ChapterListItem = ({id, name, releaseDate}: IChapterDTO) => {
+
     return (
-        <div>
-            
+        <div className={styles.main_block}>
+            <div className={styles.main_block__wrapper}>
+                <p>{id}</p>
+                <p>{name}</p>
+                <div className={styles.buttons}>
+                    <Link to={`chapters/${id}/edit`} className={styles.change}>Изменить</Link>
+                    <p className={styles.delete}>Удалить</p>
+                </div>
+            </div>
         </div>
     );
 };
