eb77b71aede1655bb581ebc041b9cc601c3c7a9c
lx1cx <lagunov.2019@list.ru>
2023-06-17 03:23:44 +0500

    Созданы методы для получения списка всех глав и для обновления текста главы

--- src/services/ChapterService.ts
+++ src/services/ChapterService.ts
@@ -1,8 +1,9 @@
 import {IError} from "./models/IError";
-import {ICreateChapterRequest} from "./models/requests/IChapterRequests";
+import {ICreateChapterRequest, IUpdateChapterRequest} from "./models/requests/IChapterRequests";
 import {ErrorService} from "./ErrorService";
 import axios from "axios";
 import {TokenService} from "./TokenService";
+import {IGetChapters} from "./models/responses/IChapterResponses";
 
 /**
  * Сервис для работы с главами
@@ -23,6 +24,48 @@ export class ChapterService {
             })
 
             return null;
+        }
+        catch (err: any) {
+            return ErrorService.toServiceError(err, "ChapterService")
+        }
+    }
+
+    /**
+     * Метод для получения списка книг
+     * @param bookId id книги
+     * @returns IGetChapters или IError
+     */
+    static async getChaptersAsync(bookId: number): Promise<IGetChapters | IError> {
+        try {
+            const response = await axios.get(process.env.REACT_APP_URL_API + `/books/${bookId}/chapters`, {
+                headers: {
+                    Authorization: await TokenService.getAccessTokenAsync()
+                }
+            })
+
+            return response.data
+        }
+        catch (err: any) {
+            return ErrorService.toServiceError(err, "ChapterService")
+        }
+    }
+
+    /**
+     * Метод для обновления текста главы
+     * @param bookId id книги
+     * @param chapterId id главы
+     * @param data данные необходимые для обновления книги
+     * @returns IError или null
+     */
+    static async updateChapterTextAsync(bookId: number, chapterId: number, data: IUpdateChapterRequest): Promise<IError| null>  {
+        try {
+            await axios.put(process.env.REACT_APP_URL_API + `/books/${bookId}/chapters/${chapterId}/text`, data, {
+                headers: {
+                    Authorization: await TokenService.getAccessTokenAsync()
+                }
+            })
+
+            return null
         }
         catch (err: any) {
             return ErrorService.toServiceError(err, "ChapterService")
