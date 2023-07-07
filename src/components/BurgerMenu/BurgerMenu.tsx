72a602727d1431becab205e5399cb54e95861b15
lx1cx <lagunov.2019@list.ru>
2023-07-07 16:47:58 +0500

    Сделано бургер меню

---
+++ src/components/BurgerMenu/BurgerMenu.tsx
@@ -0,0 +1,29 @@
+import React, {useState} from 'react';
+import styles from "./burgerMenu.module.css"
+import {TokenService} from "../../services/TokenService";
+
+const BurgerMenu = () => {
+
+    const [isOpen, setIsOpen] = useState<boolean>(false)
+
+    return (
+        <div className={styles.burger_menu}>
+            <div className={styles.btn} onClick={() => setIsOpen(!isOpen)}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <div className={isOpen ? styles.menu : styles.menu_active}>
+                <p className={styles.login}>LxCx</p>
+                <div className={styles.list}>
+                    <p className={styles.list_item}>Избранное</p>
+                    <p className={styles.list_item}>Библиотека</p>
+
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+export default BurgerMenu;
