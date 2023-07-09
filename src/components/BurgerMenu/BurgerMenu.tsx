591f10a6445e6a199bf0b6b93d99ca2bfe99fd56
lx1cx <lagunov.2019@list.ru>
2023-07-09 10:57:25 +0500

    Добавлено отображение ника

--- src/components/BurgerMenu/BurgerMenu.tsx
+++ src/components/BurgerMenu/BurgerMenu.tsx
@@ -1,9 +1,30 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import styles from "./burgerMenu.module.css"
+import {IAccessTokenData} from "../../services/models/DTO/ITokenModels";
+import {TokenService} from "../../services/TokenService";
 
 const BurgerMenu = () => {
 
     const [isOpen, setIsOpen] = useState<boolean>(false)
+    const [userData, setUserData] = useState<IAccessTokenData | null>(null)
+
+    useEffect(() => {
+
+        let accessToken: string
+
+        async function getAccessToken() {
+            const result = await TokenService.getAccessTokenAsync()
+
+            if (result !== null) {
+                accessToken = result
+            }
+        }
+
+        getAccessToken().then(() => {
+            const accessTokenData = TokenService.parseAccessToken(accessToken)
+            setUserData(accessTokenData)
+        })
+    }, [])
 
     return (
         <div className={styles.burger_menu} onClick={() => setIsOpen(!isOpen)}>
@@ -13,12 +34,17 @@ const BurgerMenu = () => {
                 <span></span>
             </div>
             <div className={isOpen ? styles.menu : styles.menu_active}>
-                <p className={styles.login}>LxCx</p>
-                <div className={styles.list}>
-                    <p className={styles.list_item}>Избранное</p>
-                    <p className={styles.list_item}>Библиотека</p>
-
-                </div>
+                {userData !== null
+                    ? <div>
+                        <p className={styles.login}>{userData.login}</p>
+                        <p className={styles.list_item}>Избранное</p>
+                    </div>
+                    : <div className={styles.list}>
+                        <p className={styles.list_item}>Вход</p>
+                        <p className={styles.list_item}>Регистрация</p>
+                    </div>
+                }
+                <p className={styles.list_item}>Библиотека</p>
 
             </div>
         </div>
