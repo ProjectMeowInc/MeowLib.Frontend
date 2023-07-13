cf9b2af2006ddcf38f13db18e645b090f56aa290
lx1cx <lagunov.2019@list.ru>
2023-07-13 15:52:57 +0500

    Реализована главная страница

--- src/pages/UserPages/MainPage/MainPage.tsx
+++ src/pages/UserPages/MainPage/MainPage.tsx
@@ -1,18 +1,61 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import styles from "./mainPage.module.css";
+import {IBookDTO} from "../../../services/models/DTO/IBookDTO";
+import {BookService} from "../../../services/BookService";
+import BookItemV1 from "../../../components/BookItemV1/BookItemV1";
+import BookItemV2 from "../../../components/BookItemV2/BookItemV2";
 
 const MainPage = () => {
 
-    return (
-        <div className={styles.main}>
-            <div className={styles.main_left}>
-                <p>Дарим замуррчательное настроение</p>
-            </div>
-            <div className={styles.main_right}>
+    const [bookList, setBookList] = useState<IBookDTO[] | null>()
 
-                <p>Последние обновления</p>
+    useEffect(() => {
+        BookService.getBooksAsync().then(getBooksResult => {
+            if (getBooksResult.tryCatchError()) {
+                return
+            }
+
+            const books = getBooksResult.unwrap()
+
+            console.log(getBooksResult);
+
+            return setBookList(books.items)
+        })
+
+    }, [])
+
+    return (
+        <div>
+            <div className={styles.main}>
+                <div className={styles.main_left}>
+                    <p>Дарим замуррчательное настроение</p>
+                </div>
+                <div className={styles.main_right}>
+
+                    <p>Недавно добавленные</p>
+
+                    <div className={styles.books}>
+                        {
+                            bookList && bookList.map(book => (
+                                <BookItemV1 key={book.id} id={book.id} name={book.name} description={book.description} imageName={book.imageName}/>
+                            ))
+                        }
+                    </div>
+                </div>
+            </div>
+            <div className={styles.wrapper}>
+                <h1 className={styles.update_caption}>Последние обновления</h1>
+
+                <div className={styles.last_updates}>
+                    {
+                        bookList && bookList.map(book => (
+                            <BookItemV2 key={book.id} id={book.id} name={book.name} description={book.description} imageName={book.imageName}/>
+                        ))
+                    }
+                </div>
             </div>
         </div>
+
     );
 };
 
