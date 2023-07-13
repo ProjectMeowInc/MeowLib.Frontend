ab5daff5b22e59354e0df9237a337db4ae8c9dca
lx1cx <lagunov.2019@list.ru>
2023-07-13 15:54:21 +0500

    Сделан компонент для вывода книги

--- src/components/BookItemV1/BookItemV1.tsx
+++ src/components/BookItemV1/BookItemV1.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import {IBookDTO} from "../../services/models/DTO/IBookDTO";
+import styles from "./bookItemV1.module.css"
 
-const BookItemV1 = () => {
+const BookItemV1 = ({id, name, description}: IBookDTO) => {
     return (
-        <div>
-            
+        <div className={styles.book}>
+            <p className={styles.caption}>{name}</p>
+            <p className={styles.description}>{description}</p>
         </div>
     );
 };
