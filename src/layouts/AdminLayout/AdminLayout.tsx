fb3af020683f3e5ba8480d10fc59c08f6cfd9e87
lx1cx <lagunov.2019@list.ru>
2023-07-15 01:58:44 +0500

    Было переписано под AuthorizationContext

--- src/layouts/AdminLayout/AdminLayout.tsx
+++ src/layouts/AdminLayout/AdminLayout.tsx
@@ -1,52 +1,22 @@
-import React, {useEffect, useState} from 'react';
+import React, {useContext} from 'react';
 import styles from "./adminLayout.module.css";
-import {Outlet, useNavigate} from "react-router-dom";
-import {TokenService} from "../../services/TokenService";
-import {AlertService} from "../../services/AlertService";
-import {IAccessTokenData} from "../../services/models/DTO/ITokenModels";
+import {Outlet} from "react-router-dom";
 import {RedirectService} from "../../services/RedirectService";
+import {AuthorizationContext} from "../../context/AuthorizationContext";
 
 const AdminLayout = () => {
+    const {user} = useContext(AuthorizationContext)
 
-    const navigate = useNavigate()
-    const [adminData, setAdminData] = useState<IAccessTokenData | null>(null)
-
-    useEffect(() => {
-
-        let accessToken: string;
-
-        async function fetchData(): Promise<void> {
-            const getAccessTokenResult = await TokenService.getAccessTokenAsync()
-
-            if (getAccessTokenResult.tryCatchError()) {
-                return
-            }
-
-            accessToken = getAccessTokenResult.unwrap()
-        }
-
-        fetchData().then(() => {
-            const accessTokenData = TokenService.parseAccessToken(accessToken)
-
-            if(accessTokenData === null) {
-                AlertService.errorMessage("Ошибка токена. Пожалуйста авторизуйтесь заново.")
-                return RedirectService.redirectToLogin()
-            }
-
-            if(accessTokenData.userRole !== "Admin" && accessTokenData.userRole !== "Moderator") {
-                return RedirectService.redirectToLogin()
-            }
-
-            setAdminData(accessTokenData)
-        })
-    }, [navigate])
+    if (user && user.hasAdminAccess()) {
+        RedirectService.redirectToLogin()
+    }
 
     return (
         <div className={styles.wrapper}>
             <div className={styles.content}>
                 <div className={styles.header}>
-                    <p onClick={() => navigate("/admin")} className={styles.logo}>MeowLib</p>
-                    <p className={styles.name}>{adminData && adminData.login}</p>
+                    <p onClick={() => RedirectService.redirect("/admin")} className={styles.logo}>MeowLib</p>
+                    <p className={styles.name}>{user && user.login}</p>
                 </div>
                 <Outlet/>
             </div>
