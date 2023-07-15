226001114fe5337fabf80e09d7379c46d4859765
lx1cx <lagunov.2019@list.ru>
2023-07-15 18:45:19 +0500

    Сделан переход на другую страницу

--- src/components/UI/LibraryBookItem/LibraryBookItem.tsx
+++ src/components/UI/LibraryBookItem/LibraryBookItem.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import {IBookDTO} from "../../../services/models/DTO/IBookDTO";
 import styles from "./libraryBookIten.module.css";
+import {Link} from "react-router-dom";
 
-const LibraryBookItem = ({name, imageName}: IBookDTO) => {
+const LibraryBookItem = ({id, name, imageName}: IBookDTO) => {
 
     return (
-        <div className={styles.book}>
+        <Link to={`${id}`} className={styles.book}>
             {
                 imageName !== null
                     ? <div style={{backgroundImage: `url(https://localhost:7007/api/images/book/${imageName})`}} className={styles.img}></div>
                     : <div className={styles.img_not_found}> Нет изображения</div>
             }
             <p className={styles.book_caption}>{name}</p>
-        </div>
+        </Link>
     );
 };
 
