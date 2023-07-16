423082bc26f9c0b6fe2244d70c45af49e5637ee2
lx1cx <lagunov.2019@list.ru>
2023-07-16 22:20:18 +0500

    Написан контекст для настроек пользователя

---
+++ src/context/SettingsContext.tsx
@@ -0,0 +1,42 @@
+import {createContext, Dispatch, ReactNode, SetStateAction, useEffect, useState} from "react";
+import IUserSettings from "../services/settings/UserSettings";
+import SettingsService from "../services/SettingsService";
+import {set} from "js-cookie";
+
+export const SettingsContext = createContext<ISettingsContext>({} as  ISettingsContext)
+
+interface ISettingsContextProviderProps {
+    children: ReactNode
+}
+
+interface ISettingsContext {
+    settings: IUserSettings
+    fontSize: number
+    setFontSize: Dispatch<SetStateAction<number>>
+    lineHeight: number
+    setLineHeight: Dispatch<SetStateAction<number>>
+}
+
+export function SettingsContextProvider({children}: ISettingsContextProviderProps) {
+
+    const settings = SettingsService.getSettings()
+
+    const [fontSize, setFontSize] = useState<number>(settings.reader.fontSize)
+    const [lineHeight, setLineHeight] = useState<number>(settings.reader.lineHeight)
+
+    useEffect(() => {
+        SettingsService.updateUserSettings({
+            theme: settings.theme,
+            reader: {
+                fontSize: fontSize,
+                lineHeight: lineHeight
+            }
+        })
+    }, [fontSize, lineHeight])
+
+    return (
+        <SettingsContext.Provider value={{settings: settings, fontSize: fontSize, setFontSize: setFontSize, lineHeight: lineHeight, setLineHeight: setLineHeight}}>
+            {children}
+        </SettingsContext.Provider>
+    )
+}
