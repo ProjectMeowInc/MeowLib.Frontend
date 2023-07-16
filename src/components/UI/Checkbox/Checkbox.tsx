1f405a34572f551ee991f4d28d0060a93a101782
BlackSoulHub <mr.asaglaev@mail.ru>
2023-07-17 02:18:13 +0300

    Сделан UI-компонент `Checkbox`

---
+++ src/components/UI/Checkbox/Checkbox.tsx
@@ -0,0 +1,36 @@
+import React, {useState} from 'react';
+import styles from "./checkbox.module.css"
+
+interface ICheckboxProps {
+    onClick?: (state: boolean) => void
+    text?: string
+}
+
+const Checkbox = ({text, onClick}: ICheckboxProps) => {
+    const [isChecked, setIsChecked] = useState<boolean>(false)
+
+    function ClickHandler(newState: boolean) {
+        setIsChecked(newState)
+
+        onClick?.call(null, newState)
+    }
+
+    return (
+        <div className={styles.checkbox_wrapper}>
+            <div
+                className={isChecked ? styles.checkbox_checked : styles.checkbox}
+                onClick={() => ClickHandler(!isChecked)}
+            >
+            </div>
+            {
+                text && (
+                    <div className={styles.text}>
+                        {text}
+                    </div>
+                )
+            }
+        </div>
+    );
+};
+
+export default Checkbox;
---
+++ src/services/models/components/IBaseStyles.ts
@@ -0,0 +1,9 @@
+
+export default interface IBaseStyles {
+    color?: string
+    margin?: string
+    marginTop?: number
+    marginBottom?: number
+    marginLeft?: number
+    marginRight?: number
+}
