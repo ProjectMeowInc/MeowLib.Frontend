c81a9a31f5ef824a03b73ef2d8adf138ecd9c24c
lx1cx <lagunov.2019@list.ru>
2023-07-18 19:56:28 +0500

    Логика вынесена в отдельный компонент

--- src/components/pages/UserPages/SettingsPage/UI/ChangeTheme/ChangeTheme.tsx
+++ src/components/pages/UserPages/SettingsPage/UI/ChangeTheme/ChangeTheme.tsx
@@ -1,30 +1,15 @@
-import React, {useContext} from 'react';
-import {SettingsContext} from "../../../../../../context/SettingsContext";
+import React from 'react';
 import styles from "./changeTheme.module.css"
-import {Themes} from "../../../../../../services/settings/UserSettings";
+import ThemeItem from "../ThemeItem/ThemeItem";
 
 const ChangeTheme = () => {
-
-    const {theme, setThemes} = useContext(SettingsContext)
-
-    function ChangeTheme(theme: Themes) {
-        setThemes(theme)
-    }
 
     return (
         <div className={styles.themes}>
             <h1>Смена темы</h1>
             <div className={styles.select}>
-                <div onClick={() => ChangeTheme("white")}
-                     className={theme === "white" ? styles.active : styles.none}
-                >
-                    Светлая
-                </div>
-                <div onClick={() => ChangeTheme("black")}
-                     className={theme === "black" ? styles.active : styles.none}
-                >
-                    Тёмная
-                </div>
+                <ThemeItem newTheme={"white"} text={"Светлая"}/>
+                <ThemeItem newTheme={"black"} text={"Тёмная"}/>
             </div>
         </div>
     );
