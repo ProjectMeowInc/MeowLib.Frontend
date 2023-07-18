1d79515c735e94a878bee0029bc2e4c0ba274a96
lx1cx <lagunov.2019@list.ru>
2023-07-18 16:48:15 +0500

    Рефакторинг

--- src/layouts/UserLayout/UI/Menu/Menu.tsx
+++ src/layouts/UserLayout/UI/Menu/Menu.tsx
@@ -11,9 +11,9 @@ const Menu = () => {
         <div className={styles.menu}>
             <MenuItem text={"Библиотека"} path={""} />
             <MenuItem text={"Избранное"} path={"favorites"}/>
-            {user
-                && user.hasAdminAccess()
-                && <MenuItem text={"Админ панель"} path={"/admin"}/>
+            {user && user.hasAdminAccess() && (
+                    <MenuItem text={"Админ панель"} path={"/admin"}/>
+                )
             }
         </div>
     );
