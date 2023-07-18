4ce5550a6315dae5cc4d4ca5eac0781da23ea48a
lx1cx <lagunov.2019@list.ru>
2023-07-18 19:55:09 +0500

    Рефакторинг

--- src/components/pages/UserPages/SettingsPage/SettingsPage.tsx
+++ src/components/pages/UserPages/SettingsPage/SettingsPage.tsx
@@ -1,23 +1,23 @@
-import React, {useContext} from 'react';
+import React from 'react';
 import styles from "./settings.module.css"
-import {AuthorizationContext} from "../../../../context/AuthorizationContext";
 import LayoutContentItem from "../../../UI/LayoutContentItem/LayoutContentItem";
 import ChangePassword from "./UI/ChangePassword/ChangePassword";
 import ChangeTheme from "./UI/ChangeTheme/ChangeTheme";
+import {useAuthorization} from "../../../../hooks/useAuthorization";
 
 const SettingsPage = () => {
 
-    const {user} = useContext(AuthorizationContext)
+    const {user} = useAuthorization()
 
     return (
         <div className={styles.settings}>
-            <LayoutContentItem style={{width: 40}}>
+            <LayoutContentItem>
                 {user && (
                     <ChangePassword/>
                 )}
             </LayoutContentItem>
 
-            <LayoutContentItem style={{width: 40}}>
+            <LayoutContentItem>
                 <ChangeTheme/>
             </LayoutContentItem>
         </div>
