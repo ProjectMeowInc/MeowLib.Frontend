ff855d2c8094a4a8dcca62aedb158ccb090d26a5
lx1cx <lagunov.2019@list.ru>
2023-07-20 17:40:15 +0500

    Создан компонент главы

---
+++ src/components/pages/UserPages/BookPage/UI/BookChapter/BookChapter.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from "./bookChapter.module.css"
+
+interface IBookChapterProps {
+    nameChapter: string
+    releaseDate: Date
+}
+
+const BookChapter = ({nameChapter, releaseDate}: IBookChapterProps) => {
+
+    const date = new Date(releaseDate)
+
+    return (
+        <div className={styles.chapter}>
+            <div className={styles.wrapper}>
+                <p>{nameChapter}</p>
+                <p>{date.toLocaleString("ru", {dateStyle: "short"})}</p>
+            </div>
+        </div>
+    );
+};
+
+export default BookChapter;
