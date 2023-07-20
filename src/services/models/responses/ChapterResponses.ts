3cbbbd41cd2f266cdc2a79f49b18cd77a85b4647
lx1cx <lagunov.2019@list.ru>
2023-07-20 18:09:28 +0500

    Изменён тип возвращаемого значения

--- src/services/models/responses/ChapterResponses.ts
+++ src/services/models/responses/ChapterResponses.ts
@@ -4,5 +4,9 @@ import {IChapterDto} from "../entities/ChapterModels";
  * Интерфейс описывающий возвращаемый список глав
  */
 export interface IGetChaptersResponse {
-    items: IChapterDto[]
+    items: {
+        id: number
+        name: string
+        releaseDate: string
+    }[]
 }
