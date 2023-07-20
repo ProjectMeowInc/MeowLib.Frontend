fb8541fbe29fdf8944c6c6293826b413390c99c6
lx1cx <lagunov.2019@list.ru>
2023-07-20 18:11:06 +0500

    Fix warning

--- src/components/pages/UserPages/BookPage/BookPage.tsx
+++ src/components/pages/UserPages/BookPage/BookPage.tsx
@@ -1,7 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {useParams} from "react-router-dom";
 import {IBook} from "../../../../services/models/entities/BookModels";
-import {IChapterDto} from "../../../../services/models/entities/ChapterModels";
 import styles from "./bookPage.module.css"
 import LayoutContentColumn from "../../../UI/LayoutContentColumn/LayoutContentColumn";
 import BookDescription from "./UI/BookDescription/BookDescription";
@@ -13,7 +12,6 @@ import BookChapters from "./UI/BookChapters/BookChapters";
 
 const BookPage = () => {
     const [book, setBook] = useState<IBook| null>(null)
-    const [chapters, setChapters] = useState<IChapterDto[] | null>(null)
     const params = useParams()
     const bookId = params.bookId
 
