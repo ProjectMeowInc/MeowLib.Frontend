8c087e1a001c0b7d8a1a4871745e382f253126c1
lx1cx <lagunov.2019@list.ru>
2023-08-29 15:11:00 +0500

    Сделан инпут с иконкой

---
+++ src/components/UI/InputWithIcon/InputWithIcon.tsx
@@ -0,0 +1,40 @@
+import React, {FC} from 'react';
+import styles from "./inputWithIcon.module.css"
+
+interface InputWithIconProps {
+    image: string
+    inputType: "text" | "password" | "email"
+    placeholder?: string
+    label?: string
+    onChange?: () => void
+}
+
+const InputWithIcon: React.FC<InputWithIconProps> = (
+    {
+        image,
+        inputType,
+        onChange,
+        placeholder,
+        label
+    }) => {
+    return (
+        <div className={styles.input_with_icon}>
+            <p className={styles.input_label}>{label}</p>
+            <div className={styles.content}>
+                <img
+                    className={styles.img}
+                    src={"/img/" + image} alt=""
+                />
+
+                <input
+                    placeholder={placeholder ?? "Начните вводить"}
+                    className={styles.input}
+                    onChange={onChange}
+                    type={inputType}
+                />
+            </div>
+        </div>
+    );
+};
+
+export default InputWithIcon;
